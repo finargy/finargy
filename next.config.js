4de339824c0652662cf980622198fcea01999dc2
marianochavez <amarianochavez@gmail.com>
2022-08-24 21:14:07 -0300

    eslint config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  // images: {
+  //   domains: ["res.cloudinary.com", "localhost", "tesla-shop-nextjs.vercel.app"],
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
